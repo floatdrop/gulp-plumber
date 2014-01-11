a2ee997865e0ab914b95df9eb7efd1ec0dd6f523
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-11 09:38:16 +0600

    Remove default onerror handler in onerror2 event
    Closes #7

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function wrapPanicOnErrorHandler(stream) {
     var oldHandler = removeDefaultHandler(stream, 'error');
     if (oldHandler) {
         stream.on('error', function onerror2(er) {
+            removeDefaultHandler(stream, 'error');
             if (EE.listenerCount(stream, 'error') === 1) {
                 this.removeListener('error', onerror2);
                 oldHandler.call(stream, er);
