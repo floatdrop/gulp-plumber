c542e119dfd32bfcf80993adeb36c09ca1477927
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-02-09 16:31:56 +0500

    Remove readable handler

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ function plumber(opts) {
 			wrapPanicOnErrorHandler(stream);
 			stream._pipe = stream._pipe || stream.pipe;
 			stream.pipe = stream.pipe2;
-			stream.once('readable', patchPipe.bind(null, stream));
 			stream._plumbed = true;
 		}
 	}
